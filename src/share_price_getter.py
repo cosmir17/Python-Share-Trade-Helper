22d55c3deff373ca072a8928003b86a1876d73e4
sk <sk.sk>
2016-11-19 18:46:05 +0000

    working on producing a dictionary of daily dataframe objects.

--- src/share_price_getter.py
+++ src/share_price_getter.py
@@ -26,17 +26,6 @@ class SharePriceGetter():
         window = days_between_dates.days
         return __google_api_get_price__(self.share_symbol, self.sec_interval, window)
 
-    def getCurrentPrice(self):
-        self.share.refresh()
-        return self.share.get_price()
-
-    def plot_prices(prices):
-        plt.title('Opening stock prices')
-        plt.xlabel('day')
-        plt.ylabel('price ($)')
-        plt.plot(prices)
-        plt.savefig('prices.png')
-
 def __google_api_get_price__(share_symbol, sec_interval, window):
     url_root = 'http://www.google.com/finance/getprices?i='
     url_root += str(sec_interval) + '&p=' + str(window)
@@ -58,15 +47,58 @@ def __google_api_get_price__(share_symbol, sec_interval, window):
             coffset = int(cdata[0])
             cts = int(anchor_stamp) + (coffset * sec_interval)
 
-            yesterday = dt.date.today() - dt.timedelta(1)
-            unix_time = yesterday.strftime("%s")  # Second as a decimal number [00,61] (or Unix Timestamp)
-            dt.datetime.fromtimestamp(float(cts))
-
-            parsed_data.append((
-                               dt.datetime.fromtimestamp(float(cts)), float(cdata[1]), float(cdata[2]), float(cdata[3]),
-                               float(cdata[4]), float(cdata[5])))
+            # yesterday = dt.date.today() - dt.timedelta(1)
+            # unix_time = yesterday.strftime("%s")  # Second as a decimal number [00,61] (or Unix Timestamp)
+            now_time = dt.datetime.fromtimestamp(float(cts))
+            # now_time_in_format = dt.unicode(now_time)
+            parsed_data.append((cts, now_time, float(cdata[1]), float(cdata[2]), float(cdata[3]), float(cdata[4]), float(cdata[5])))
     df = pd.DataFrame(parsed_data)
-    df.columns = ['ts', 'o', 'h', 'l', 'c', 'v']
-    df.index = df.ts
-    del df['ts']
+    df.columns = ['tstamp', 'datetime', 'o', 'h', 'l', 'c', 'v'] #date_time open high low close volume
+    # df.index = df.ts
+    # del df['ts']
+
+    timestamp_oneday = 86400
+
+    daily_stock_dictionary = {}
+    df_next_index = 0
+    df_length = len(df.index)
+
+    # for i in range(df.size):
+    while df_next_index < df_length:
+        initial_timestamp = df.get_value(df_next_index, 'tstamp')
+        temp_datetime = dt.datetime.fromtimestamp(initial_timestamp)
+        day_plus_timestamp = initial_timestamp + timestamp_oneday
+        mask = (df['tstamp'] > initial_timestamp) & (df['tstamp'] <= day_plus_timestamp)
+        dframes = df.loc[mask]
+        daily_stock_dictionary[temp_datetime] = dframes
+        dframe_length = len(dframes.index)
+        temp_df_lastindex_timestamp = dframes.get_value(dframe_length, 'tstamp')
+        df_next_index += dframe_length
+
+    print (daily_stock_dictionary)
+             # = dframes.get_value(lastindex_number, 'tstamp')
+
+
+
+
+    # dframes.
+       # for j in range(j, df.size):
+       #   j_timestamp = df.get_value(j, 'ts')
+       #
+       #   if j_timestamp < until_tomorrow:
+       #     frame1 = df.iloc[[j]]
+
     return df
+
+
+    def getCurrentPrice(self):
+        self.share.refresh()
+        return self.share.get_price()
+
+
+    def plot_prices(prices):
+        plt.title('Opening stock prices')
+        plt.xlabel('day')
+        plt.ylabel('price')
+        plt.plot(prices)
+        plt.savefig('prices.png')
