c1fe2775fd0bcf9f849e969d289b0f6433c724b8
sk <sk.sk>
2016-11-20 21:01:37 +0000

    managed to get timestamp of tslib.Timestamp.

--- src/share_price_getter.py
+++ src/share_price_getter.py
@@ -31,88 +31,15 @@ def __google_api_get_price__(share_symbol, sec_interval, window):
     start = dt.datetime(1999, 1, 10)
     end = dt.datetime(2016, 11, 19)
     df = web.DataReader("LSE:LLOY", 'google', start, end)
-    # print
-    # df.head(10)
-
-
-    # http: // www.google.com / finance / historical?q = NASDAQ:AAPL & startdate = Jan + 01 % 2C + 2000 & output = csv
-    # url_root = 'http://www.google.com/finance/historical?'
-    # url_root += 'i=' + str(sec_interval) + '&'
-    # url_root += 'q=' + 'LSE:' + share_symbol + '&'
-    # url_root += 'startdate=Jan+01%2C+2000'
-
-    # url_root += 'd&f=d,o,h,l,c,v&df=cpct&q=' + share_symbol
-    # output=csv
-    # + '&p=' + str(window)
-
-    # for
-    # url_root = 'http://www.google.com/finance/getprices?i='
-    # url_root += str(sec_interval) + '&p=' + str(window) + 'd&'
-    # url_root += 'f=d,o,h,l,c,v&df=cpct&q=' + share_symbol
-    # response = urllib.request.urlopen(url_root)
-    # data = response.read().split(b'\n')
-    #
-    # parsed_data = []
-    #
-    # for i in range(7, len(data) - 1):
-    #     cdata = data[i].split(b',')
-    #     cdata0 = cdata[0]
-    #     if b'a' in cdata0:  # first one record anchor timestamp
-    #         anchor_stamp = cdata0.replace(b'a', b'')
-    #         cts = int(anchor_stamp)
-    #     elif b'TIMEZONE_OFFSET=0' or b'TIMEZONE_OFFSET=60' in cdata0:
-    #         pass
-    #     else:
-    #         coffset = int(cdata[0])
-    #         cts = int(anchor_stamp) + (coffset * sec_interval)
-    #
-    #         # yesterday = dt.date.today() - dt.timedelta(1)
-    #         # unix_time = yesterday.strftime("%s")  # Second as a decimal number [00,61] (or Unix Timestamp)
-    #         now_time = dt.datetime.fromtimestamp(float(cts))
-    #         # now_time_in_format = dt.unicode(now_time)
-    #         parsed_data.append((cts, now_time, float(cdata[1]), float(cdata[2]), float(cdata[3]), float(cdata[4]), float(cdata[5])))
-    # df = pd.DataFrame(parsed_data)
-    # df.columns = ['datetime', 'o', 'h', 'l', 'c', 'v'] #date_time open high low close volume
-    # df.index = df.ts
-    # del df['ts']
-
-
+    ts_list = df.index.tolist()  # a list of Timestamp's
+    date_list = [int(round(ts.to_datetime().timestamp())) for ts in ts_list]  # a list of datetime.date's
+    date_str_list = [str(date) for date in date_list]  # a list of strings
+    df['time_stamp'] = date_str_list
     df['index_c'] = range(0, len(df))
     df['d_time'] = df.index
     df.index = df.index_c
-
-    timestamp_oneday = 86400
-
-    daily_stock_dictionary = {}
-    df_next_index = 0
     df_length = len(df.index)
-
-
     last_day = df.get_value(df_length-1, 'd_time')
-
-    # while df_next_index < df_length:
-    #     initial_day = df.get_value(df_next_index, 'd_time')
-    #     # temp_datetime = dt.datetime.fromtimestamp(initial_day)
-    #     # day_plus_timestamp = initial_day + timestamp_oneday
-    #     # mask = (df['tstamp'] > initial_day) & (df['tstamp'] <= day_plus_timestamp)
-    #     # dframes = df.loc[mask]
-    #     daily_stock_dictionary[temp_datetime] = dframes
-    #     dframe_length = len(dframes.index)
-    #     temp_df_lastindex_timestamp = dframes.get_value(dframe_length-1, 'tstamp')
-    #     df_next_index += dframe_length
-    #
-    # print (daily_stock_dictionary)
-             # = dframes.get_value(lastindex_number, 'tstamp')
-
-
-
-
-    # dframes.
-       # for j in range(j, df.size):
-       #   j_timestamp = df.get_value(j, 'ts')
-       #
-       #   if j_timestamp < until_tomorrow:
-       #     frame1 = df.iloc[[j]]
 
     return df
 
